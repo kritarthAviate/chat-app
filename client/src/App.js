d64f809d9069f5e59e742e49c06bc60dca77e13a
Alien Coder <paidcodecamp@gmail.com>
2023-04-07 20:02:33 +0530

    added socket.io-client to create connection with server

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,8 @@
+import socketIO from "socket.io-client";
+const socket = socketIO("http://localhost:8080");
+
 function App() {
+  console.log({ socket });
   return (
     <div>
       <p>Hello World!</p>
