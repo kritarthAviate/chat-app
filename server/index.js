a0e6d132afd984d3390c9748cf549f59e948228f
Alien Coder <paidcodecamp@gmail.com>
2023-04-07 20:02:00 +0530

    updated: BE to support socket connection from client

--- server/index.js
+++ server/index.js
@@ -1,7 +1,40 @@
-const app = require("./app");
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const app = express();
+const http = require("http").Server(app);
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, function () {
-  console.log(`Express app listening on port ${PORT}`);
+dotenv.config();
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(helmet());
+const corsOptions = {
+  credentials: true,
+  origin: true,
+  allowedHeaders: "content-type, origin, accept,",
+  methods: "GET,POST,PUT",
+};
+app.use(cors(corsOptions));
+
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+//Add this before the app.get() block
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
