eb499edf4395dbe0e5a7043b1c47af3ccefb84c9
Alien Coder <paidcodecamp@gmail.com>
2023-04-08 16:12:56 +0530

    added netlify config, updated css

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 const Home = ({ socket }) => {
   const navigate = useNavigate();
   const [userName, setUserName] = useState("");
+  const disabled = !userName?.trim()?.length;
   const handleSubmit = (e) => {
     e.preventDefault();
     localStorage.setItem("userName", userName);
@@ -11,18 +12,23 @@ const Home = ({ socket }) => {
   };
   return (
     <form className="home__container" onSubmit={handleSubmit}>
-      <h2 className="home__header">Sign in to Open Chat</h2>
-      <label htmlFor="username">Username</label>
+      <h2 className="home__header">Enter Username to get started</h2>
       <input
+        autoComplete="off"
         type="text"
-        minLength={6}
+        minLength={3}
         name="username"
         id="username"
         className="username__input"
         value={userName}
         onChange={(e) => setUserName(e.target.value)}
       />
-      <button className="home__cta">SIGN IN</button>
+      <button
+        className={disabled ? "home__cta__disabled" : "home__cta"}
+        disabled={disabled}
+      >
+        Enter
+      </button>
     </form>
   );
 };
