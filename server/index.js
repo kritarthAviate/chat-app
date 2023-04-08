f48c4aac0cf2184fd7afb0f80d9e0a57124efd00
Alien Coder <paidcodecamp@gmail.com>
2023-04-09 03:10:39 +0530

    updated: server to support rooms

--- server/index.js
+++ server/index.js
@@ -29,27 +29,61 @@ const socketIO = require("socket.io")(http, {
   },
 });
 
-let users = [];
+const users = {};
+const rooms = {};
 
 // socket connection handler
 socketIO.on("connection", (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
-  socket.on("message", (data) => {
-    socketIO.emit("messageResponse", data);
+  socket.on("message", (data, room) => {
+    console.log({ users, rooms });
+    console.log({ data });
+    socketIO.to(room).emit("messageResponse", data);
   });
 
   socket.on("typing", (data) => socket.broadcast.emit("typingResponse", data));
   socket.on("stopTyping", () => socket.broadcast.emit("stopTypingResponse"));
 
-  socket.on("newUser", (data) => {
-    users.push(data);
-    socketIO.emit("newUserResponse", users);
+  // socket.on("newUser", (data) => {
+  //   users.push(data);
+  //   socketIO.emit("newUserResponse", users);
+  // });
+
+  socket.on("createRoom", (data, room) => {
+    console.log("🚀: createRoom", { data });
+    console.log({ users, rooms });
+    socket.join(room);
+    rooms[room] = [data];
+    users[data.socketID] = room;
+    socketIO.to(room).emit("createRoomResponse", rooms[room]);
+  });
+
+  socket.on("joinRoom", (data, room) => {
+    console.log("🚀: joinRoom", { data });
+    console.log({ users, rooms });
+    socket.join(room);
+    // if(data.socketID in users) return;
+    let roomUsers = rooms[room] || [];
+    rooms[room] = [...roomUsers, data];
+    users[data.socketID] = room;
+    socketIO.to(room).emit("joinRoomResponse", rooms[room]);
   });
 
   socket.on("disconnect", () => {
     console.log("🔥: A user disconnected");
-    users = users.filter((user) => user.socketID !== socket.id);
-    socketIO.emit("newUserResponse", users);
+    user = socket.id;
+    if (user in users) {
+      const roomId = users[user];
+      let room = rooms[roomId] || [];
+      console.log({ room, user }, "from disconnect");
+      delete users[user];
+      if (room?.length) {
+        room = room.filter((_user) => _user.socketID !== user);
+        console.log({ room, user }, "from disconnect2");
+        rooms[roomId] = room;
+        socketIO.to(room).emit("userLeft", room);
+      }
+    }
     socket.disconnect();
   });
 });
